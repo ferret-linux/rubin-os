80df156e8599e216312ecdce2219c85389b3c116
itzNOELdev <dev.zodium.project@gmail.com>
2026-06-15 13:15:46 +0530

    Fix : post setup

--- build_files/cleanup.sh
+++ build_files/cleanup.sh
@@ -3,8 +3,6 @@
 set -ouex pipefail
 
 ### Cleanup
-dnf5 -y copr disable matinlotfali/KDE-Rounded-Corners
-rm -rf /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:matinlotfali:KDE-Rounded-Corners.repo
 dnf config-manager setopt ferret-pkgs.enabled=0
 rm -f /etc/yum.repos.d/ferret-pkgs.repo
 dnf5 autoremove -y
